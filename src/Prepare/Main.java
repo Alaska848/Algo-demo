6adf9730338f80f1cfb4eddabec9041f64664d74
Mariam Ashraf <mariamashraf123mm@gmail.com>
2025-11-23 13:21:24 +0200

    updated

--- src/Prepare/Main.java
+++ src/Prepare/Main.java
@@ -168,6 +168,28 @@ public class Main {
       return max;
     }
 
+    /* divisor game
+    * Alice and Bob take turns playing a game, with Alice starting first.
+     Initially, there is a number n on the chalkboard.
+     * On each player's turn, that player makes a move consisting of:
+     Choosing any integer x with 0 < x < n and n % x == 0.
+     Replacing the number n on the chalkboard with n - x.
+     Also, if a player cannot make a move, they lose the game.
+     Return true if and only if Alice wins the game,
+     *  assuming both players play optimally.
+     */
 
+    public boolean divisorGame(int n) {
+        boolean [] dp = new boolean[n + 1];
 
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                if(i % j == 0 && !dp[i - j]){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
 }
